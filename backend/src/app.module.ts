498989834ab27938c729b160cce6acfb3a92ca29
Mathieu CHEREAU <63231996+matmathieu@users.noreply.github.com>
2026-05-24 12:25:38 +0200

    feat: implement US10 daily cron job to cleanup expired files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,18 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { FilesModule } from './files/files.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule, FilesModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    FilesModule,
+    ScheduleModule.forRoot(),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
